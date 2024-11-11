7a6e71bd7e3db8988f1cc2020552a33b7c4557f2
Christoph Becker <mail@ch-becker.de>
2024-11-11 11:24:32 +0100

    add rdp

---
+++ users/cb/rdp_dsktop.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+let vpnDesktopItem = pkgs.makeDesktopItem {
+    name = "VSX-RDP";
+    desktopName = "Make RDP Connection, VN neeeded";
+    exec = "/home/cb/.local/bin/rdp_vsx.sh";
+    terminal = "true";
+  };
+in {
+  home.packages = [ 
+    vpnDesktopItem 
+  ]; 
+}
